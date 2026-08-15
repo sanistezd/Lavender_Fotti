3bafcc1dacac117c41c34d1b02a1a510dc9c0cef
sanistezd <sasha2007470@gmail.com>
2026-08-15 23:34:02 +0300

    chore(seo): add description to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ const inter = Inter({
 
 export const metadata = {
   title: "Fotty Motion",
+  description: "Fotty Motion - сервис по оживлению и обработке фотографий.",
 };
 
 export const viewport = {
